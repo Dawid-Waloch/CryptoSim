3dc7fcd51cb0f4036da310f40c870fca3cdb0bde
Maszko <topmil77@gmail.com>
2025-12-05 18:28:04 +0100

    backend initialization

---
+++ cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/CryptosimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.dawidwaloch.cryptosim.cryptosim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptosimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptosimBackendApplication.class, args);
+	}
+
+}
---
+++ cryptosim-backend/src/test/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/CryptosimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dawidwaloch.cryptosim.cryptosim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptosimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
