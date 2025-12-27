d5d0ad4bd8c8e324ceabf402be3843fb1ea91af5
Dawid <dawwal1900@gmail.com>
2025-12-27 13:48:19 +0100

    ProtectedRoute added to avoid using some part of site without logging in

---
+++ frontend/components/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+    const { user } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(!user) router.replace("/login");
+
+
+    }, [user, router]);
+
+    if (!user) return null;
+
+    return <>{children}</>;
+}
+
+export default ProtectedRoute;
