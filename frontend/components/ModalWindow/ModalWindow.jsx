02b726f0e8b86dffba939140865a8ab90dbd532d
Dawid <dawwal1900@gmail.com>
2026-01-17 09:14:38 +0100

    Modal Windows apperance and styled corrected to look better

--- frontend/components/ModalWindow/ModalWindow.jsx
+++ frontend/components/ModalWindow/ModalWindow.jsx
@@ -1,10 +1,26 @@
-import { AssetInfo, Button, CancelContainer, Icon, Input, ModalContainer, OverlayContainer } from "./ModalWindowStyled"
+import { useState } from "react";
+import {
+    AssetInfo,
+    Button,
+    CancelContainer,
+    CostInfoSpan,
+    Icon,
+    Input,
+    ModalContainer,
+    OverlayContainer,
+    ErrorContainer
+} from "./ModalWindowStyled"
 
-const ModalWindow = ({ handleClose, assetName, operationType }) => {
+const ModalWindow = ({ handleClose, assetInfo, operationType, handleConfirm }) => {
+    const [quantityOfAsset, setQuantityOfAsset] = useState(null);
+    const [formError, setFormError] = useState("");
+
     const OPERATION_LABEL = {
         BUY: "Buy",
         SELL: "Sell"
     };
+
+    const label = operationType === "BUY" ? "Cost" : "Income";
 
     return (
         <OverlayContainer>
@@ -12,9 +28,19 @@ const ModalWindow = ({ handleClose, assetName, operationType }) => {
                 <CancelContainer>
                     <Icon onClick={handleClose} />
                 </CancelContainer>
-                <AssetInfo>{OPERATION_LABEL[operationType]} {assetName}</AssetInfo>
-                <Input type="number" placeholder="Amount" />
-                <Button>Confirm</Button>
+                <AssetInfo>{OPERATION_LABEL[operationType]} {assetInfo.name} ({assetInfo.currentPrice}$)</AssetInfo>
+                <Input
+                    type="number"
+                    placeholder="Quantity"
+                    name="Quantity"
+                    value={quantityOfAsset}
+                    onChange={(e) => setQuantityOfAsset(e.target.value)}
+                />
+                <ErrorContainer>{formError}</ErrorContainer>
+                <CostInfoSpan>
+                    {label}: {assetInfo.currentPrice * quantityOfAsset}$
+                </CostInfoSpan>
+                <Button onClick={() => handleConfirm(quantityOfAsset, setFormError)}>Confirm</Button>
             </ModalContainer>
         </OverlayContainer>
     );
