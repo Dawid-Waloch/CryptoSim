a1bafe4b7ab7ddc0551123173d5c61d7ab9278ac
Maszko <topmil77@gmail.com>
2026-01-18 13:45:50 +0100

    add asset price history scheduling

--- cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/CryptosimBackendApplication.java
+++ cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/CryptosimBackendApplication.java
@@ -2,8 +2,10 @@ package com.github.dawidwaloch.cryptosim.cryptosim_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CryptosimBackendApplication {
 
 	public static void main(String[] args) {
--- cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/assetHistory/AssetPriceHistoryService.java
+++ cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/assetHistory/AssetPriceHistoryService.java
@@ -1,8 +1,10 @@
 package com.github.dawidwaloch.cryptosim.cryptosim_backend.assetHistory;
 
+import com.github.dawidwaloch.cryptosim.cryptosim_backend.assets.Asset;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -13,5 +15,14 @@ public class AssetPriceHistoryService {
 
     public List<AssetPricecHistoryDTO> getAssetHistory(Long assetId){
         return assetPriceHistoryRepository.findByAssetId(assetId).stream().map(mapper::toDto).toList();
+    }
+
+    public void saveAssetPrice(Asset asset){
+        AssetPriceHistory history = new AssetPriceHistory();
+        history.setAsset(asset);
+        history.setPrice(asset.getCurrentPrice());
+        history.setRecordedAt(LocalDateTime.now());
+
+        assetPriceHistoryRepository.save(history);
     }
 }
