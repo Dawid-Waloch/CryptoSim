da67d08cba3ef15e9726833e5ec1f4b713cc012a
Dawid <dawwal1900@gmail.com>
2026-02-09 19:03:38 +0100

    Add swagger notes

--- cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/candlestick/CandlestickController.java
+++ cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/candlestick/CandlestickController.java
@@ -8,17 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Candles")
 @RestController
 @RequestMapping("/assets")
 @RequiredArgsConstructor
 public class CandlestickController {
     private final CandlestickService candlestickService;
 
+    @Operation(summary = "get candles", description = "Gets candles OHLC info for specific assetId and interval")
     @GetMapping("/{assetId}/candles")
     public List<CandlestickDTO> getCandles(
+        @Parameter(description = "Asset ID", example = "1")
         @PathVariable Long assetId,
+
+        @Parameter(description = "Interval", example = "1d")
         @RequestParam(defaultValue = "1d") String interval
     ) {
         return candlestickService.getCandlesticks(assetId, interval);
