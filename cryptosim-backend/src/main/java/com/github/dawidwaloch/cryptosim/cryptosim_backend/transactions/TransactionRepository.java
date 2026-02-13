ca1c9d014c2030b7db2a45d66fed82d4bc2d6790
Dawid <dawwal1900@gmail.com>
2026-02-13 12:18:41 +0100

    Add resetTransactions query to transactionRepository

--- cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/transactions/TransactionRepository.java
+++ cryptosim-backend/src/main/java/com/github/dawidwaloch/cryptosim/cryptosim_backend/transactions/TransactionRepository.java
@@ -1,6 +1,8 @@
 package com.github.dawidwaloch.cryptosim.cryptosim_backend.transactions;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     List<Transaction> findByUserId(Long userId);
     List<Transaction> findByUserIdAndAssetId(Long userId, Long assetId);
     List<Transaction> findByAssetIdOrderByCreatedAtAsc(Long assetId);
+    @Query("UPDATE Transaction t SET t.disabled = TRUE WHERE t.user_id = :user_id")
+    void resetTransactions(@Param("user_id") Long userId);
 }
