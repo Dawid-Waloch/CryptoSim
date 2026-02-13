99380283007a1e99df9f3513af1fff7775d27559
Dawid <dawwal1900@gmail.com>
2026-02-13 10:11:39 +0100

    Add layout and styles to profile page

--- frontend/pages/profile/index.jsx
+++ frontend/pages/profile/index.jsx
@@ -1,11 +1,40 @@
 import Navbar from "../../components/Navbar/Navbar";
 import ProtectedRoute from "../../components/ProtectedRoute"
+import { useAuth } from "../../context/AuthContext";
+import {
+    InfoSpan,
+    ProfileContainer,
+    SimulationBody,
+    SimulationCard,
+    SimulationText,
+    UserInfoBody,
+    UserInfoCard,
+    UserInfoText
+} from "./styles";
 
 const Profile = () => {
+    const { user } = useAuth();
+
+    const { username } = user || {};
+
     return (
         <ProtectedRoute>
             <Navbar />
-            <div>Profile</div>
+            <ProfileContainer>
+                <UserInfoCard>
+                    <UserInfoText>User Info:</UserInfoText>
+                    <UserInfoBody>
+                        <InfoSpan>Username: {username}</InfoSpan>
+                        <InfoSpan>E-mail: {}</InfoSpan>
+                    </UserInfoBody>
+                </UserInfoCard>
+                <SimulationCard>
+                    <SimulationText>Simulation:</SimulationText>
+                    <SimulationBody>
+                        <InfoSpan>Balance: {}</InfoSpan>
+                    </SimulationBody>
+                </SimulationCard>
+            </ProfileContainer>
         </ProtectedRoute>
     )
 }
