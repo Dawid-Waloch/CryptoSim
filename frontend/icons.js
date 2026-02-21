bbf425cc0a776d75baffc48f30705ca06572df2e
Dawid <dawwal1900@gmail.com>
2026-02-21 10:42:45 +0100

    Fix icon's import in WalletDropdown and add icon to icons file

--- frontend/icons.js
+++ frontend/icons.js
@@ -4,3 +4,4 @@ export { default as AppRegistrationIcon } from '@mui/icons-material/AppRegistrat
 export { default as StoreIcon } from '@mui/icons-material/Store';
 export { default as HomeIcon } from '@mui/icons-material/Home';
 export { default as AccountCircleIcon } from '@mui/icons-material/AccountCircle';
+export { default as AccountBalanceWalletIcon } from '@mui/icons-material/AccountBalanceWallet';
