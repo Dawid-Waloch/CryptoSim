df89c8e8648c3eff9a0e4e9998252240bfffea6c
Dawid <dawwal1900@gmail.com>
2026-02-24 20:36:52 +0100

    Add 'it' to import in AssetsTable

--- frontend/components/AssetsTable/AssetsTable.test.jsx
+++ frontend/components/AssetsTable/AssetsTable.test.jsx
@@ -1,4 +1,4 @@
-import { describe, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import userEvent from "@testing-library/user-event";
 import { customRender, screen } from "../../tests/test-utils";
 import AssetsTable from "./AssetsTable";
