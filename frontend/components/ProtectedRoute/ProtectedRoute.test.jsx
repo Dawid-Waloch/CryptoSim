574791efdcd339243dd3108ea0154d4496409da5
Dawid <dawwal1900@gmail.com>
2026-03-16 21:26:28 +0100

    Add test when user is authenticated to ProtectedRoute tests

--- frontend/components/ProtectedRoute/ProtectedRoute.test.jsx
+++ frontend/components/ProtectedRoute/ProtectedRoute.test.jsx
@@ -1,18 +1,36 @@
-import { describe, expect, it, vi } from "vitest";
-import { customRender, screen } from "../../tests/test-utils";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { render } from "../../tests/test-utils";
 import ProtectedRoute from "./ProtectedRoute";
 import { useAuth } from "../../context/AuthContext";
 
 vi.mock('../../context/AuthContext');
 
 describe('ProtectedRoute Component', () => {
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
     it('renders nothing when auth state is loading', () => {
         vi.mocked(useAuth).mockReturnValue({
             user: undefined
         });
 
-        const { container } = customRender(<ProtectedRoute>Child</ProtectedRoute>);
+        const { container } = render(
+            <ProtectedRoute>Child</ProtectedRoute>
+        );
 
         expect(container.firstChild).toBeNull();
+    });
+
+    it("renders children when user is authenticated", () => {
+        vi.mocked(useAuth).mockReturnValue({
+            user: 'John'
+        });
+
+        const { container } = render(
+            <ProtectedRoute>Child</ProtectedRoute>
+        );
+
+        expect(container.firstChild).not.toBeNull();
     });
 });
