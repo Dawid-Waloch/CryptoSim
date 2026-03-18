1240efaa1d5ce2a44ba8d9d4887085631fb12e61
Dawid <dawwal1900@gmail.com>
2026-03-18 20:15:57 +0100

    Add sample WalletDropdown test file

---
+++ frontend/components/WalletDropdown/WalletDropdown.test.jsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { customRender, screen } from "../../tests/test-utils";
+
+describe('WalletDropdown Component', () => {
+    it('', () => {
+
+    });
+});
