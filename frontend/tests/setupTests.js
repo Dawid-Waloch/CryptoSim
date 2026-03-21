eb7c5276069fbfe6857bd79298586616ae39cd2d
Dawid <dawwal1900@gmail.com>
2026-03-21 21:53:07 +0100

    Add mock directories to setupTests file

--- frontend/tests/setupTests.js
+++ frontend/tests/setupTests.js
@@ -8,5 +8,39 @@ vi.stubGlobal('fetch', vi.fn(() =>
 ));
 
 vi.mock('next/router', () => ({
-  useRouter: () => ({ push: vi.fn(), query: {} }),
+    useRouter: vi.fn(() => ({
+        push: vi.fn(),
+        replace: vi.fn(),
+        query: {},
+    }))
+}));
+
+vi.mock('react-hot-toast', () => ({
+    default: {
+        success: vi.fn(),
+        error: vi.fn(),
+    },
+}));
+
+vi.mock('../context/AuthContext', () => ({
+    useAuth: vi.fn(() => ({
+        user: {
+            userId: 1,
+            username: 'dawid',
+            email: 'dawid@wp.pl'
+        }
+    }))
+}));
+
+vi.mock('../context/ToastContext', () => ({
+    useToast: vi.fn(() => ({
+        setFlashMessage: vi.fn(),
+        clearFlashMessage: vi.fn(),
+    }))
+}));
+
+vi.mock('../context/AssetContext', () => ({
+    useAsset: vi.fn(() => ({
+        setSelectedAsset: vi.fn(),
+    }))
 }));
