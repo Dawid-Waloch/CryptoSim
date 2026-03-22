51b515946a310993eb13656427d8adf2c35dd679
Dawid <dawwal1900@gmail.com>
2026-03-22 11:27:02 +0100

    Fix matchMedia errors

--- frontend/tests/setupTests.js
+++ frontend/tests/setupTests.js
@@ -44,3 +44,17 @@ vi.mock('../context/AssetContext', () => ({
         setSelectedAsset: vi.fn(),
     }))
 }));
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(), // deprecated
+        removeListener: vi.fn(), // deprecated
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+});
