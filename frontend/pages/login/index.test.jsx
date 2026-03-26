8cc0f2d605b976b815819c74e4e8b02e45d4e0c9
Dawid <dawwal1900@gmail.com>
2026-03-26 19:36:35 +0100

    Add toast render when user not found test to Login Page Integration tests

--- frontend/pages/login/index.test.jsx
+++ frontend/pages/login/index.test.jsx
@@ -1,12 +1,26 @@
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { useToast } from "../../context/ToastContext";
+import { Toaster } from "react-hot-toast";
+import { ToastProvider } from "../../context/ToastContext";
+import FlashMessageListener from "../../components/FlashMessageListener/FlashMessageListener";
 import LoginPage from ".";
 
+vi.unmock('../../context/ToastContext');
+
 describe('Login Page Integration', () => {
-    it('renders message when inputs are not filled', async () => {
-        render(<LoginPage />);
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it('renders all kind of errors', async () => {
+        render(
+            <ToastProvider>
+                <Toaster />
+                <FlashMessageListener />
+                <LoginPage />
+            </ToastProvider>
+        );
 
         const submitButton = screen.getByRole('button', { name: /submit/i });
 
@@ -14,5 +28,34 @@ describe('Login Page Integration', () => {
         
         expect(screen.getByText(/fill the username field/i)).toBeInTheDocument();
         expect(screen.getByText(/fill the password field/i)).toBeInTheDocument();
+    });
+
+    it('renders toast when user not found', async () => {
+
+        const mockFetch = vi.spyOn(global, 'fetch').mockResolvedValueOnce({
+            ok: false,
+            status: 404,
+            json: async () => ({ message: 'User not found' }),
+        });
+        
+        render(
+            <ToastProvider>
+                <Toaster />
+                <FlashMessageListener />
+                <LoginPage />
+            </ToastProvider>
+        );
+
+        const usernameInput = screen.getByPlaceholderText('Username');
+        const passwordInput = screen.getByPlaceholderText('Password');
+        const submitButton = screen.getByRole('button', { name: /submit/i });
+
+        await userEvent.type(usernameInput, 'da');
+        await userEvent.type(passwordInput, 'da');
+        await userEvent.click(submitButton);
+
+        expect(await screen.findByText(/user not found/i)).toBeInTheDocument();
+
+        mockFetch.mockRestore();
     });
 });
