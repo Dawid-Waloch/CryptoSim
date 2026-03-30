00025fabe79a86e52c52ec5449af70770689edaa
Dawid <dawwal1900@gmail.com>
2026-03-30 23:20:32 +0200

    Add sample Profile Page Integration test file

---
+++ frontend/pages/profile/index.test.jsx
@@ -0,0 +1,9 @@
+import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import ProfileContainer from "../../components/ProfileContainer/ProfileContainer";
+
+describe('Profile Page Integration', () => {
+    it('', () => {
+        
+    });
+});
