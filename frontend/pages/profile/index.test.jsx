5ee4e21e1a3500845143f53c683639f97acf6506
Dawid <dawwal1900@gmail.com>
2026-03-30 23:25:30 +0200

    Add renders main profile elements test

--- frontend/pages/profile/index.test.jsx
+++ frontend/pages/profile/index.test.jsx
@@ -3,7 +3,22 @@ import { render, screen } from "@testing-library/react";
 import ProfileContainer from "../../components/ProfileContainer/ProfileContainer";
 
 describe('Profile Page Integration', () => {
-    it('', () => {
-        
+    const mockUSDWallet = { balance: 90, currency: 'USD', id: 9, isActive: true, lockedBalance: 0 };
+
+    const mockHandleLogout = vi.fn();
+
+    const mockResetSimulation = vi.fn();
+
+    it('renders main profile elements', () => {
+        render(
+            <ProfileContainer
+                handleLogout={mockHandleLogout}
+                usdWallet={mockUSDWallet}
+                resetSimulation={mockResetSimulation}
+            />
+        );
+
+        expect(screen.getByText(/user info:/i)).toBeInTheDocument();
+        expect(screen.getByText(/simulation:/i)).toBeInTheDocument();
     });
 });
