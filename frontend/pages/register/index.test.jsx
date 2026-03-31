650049cbcdf6604992b72b57f74953782d1874bd
Dawid <dawwal1900@gmail.com>
2026-03-31 20:51:14 +0200

    Add sample Register Page Integration test file

---
+++ frontend/pages/register/index.test.jsx
@@ -0,0 +1,8 @@
+import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+describe('Register Page Integration', () => {
+    it('', () => {
+
+    });
+});
