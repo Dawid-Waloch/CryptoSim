40c0a9395494f984b08cd24bc145300dcc82f8aa
Dawid <dawwal1900@gmail.com>
2026-03-31 21:05:07 +0200

    Add renders all kind of errors test

--- frontend/pages/register/index.test.jsx
+++ frontend/pages/register/index.test.jsx
@@ -1,8 +1,21 @@
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import RegisterPage from ".";
+import userEvent from "@testing-library/user-event";
 
 describe('Register Page Integration', () => {
-    it('', () => {
+    it('renders all kind of errors', async () => {
+        render(<RegisterPage />);
 
+        const repeatPasswordInput = screen.getByPlaceholderText('Password');
+        const submitButton = screen.getByRole('button', { name: /submit/i });
+        
+        await userEvent.type(repeatPasswordInput, 'a');
+        await userEvent.click(submitButton);
+
+        expect(screen.getByText(/fill the username field/i)).toBeInTheDocument();
+        expect(screen.getByText(/your email has to include @/i)).toBeInTheDocument();
+        expect(screen.getByText(/your password has to contain more than 7 characters/i)).toBeInTheDocument();
+        expect(screen.getByText(/you password and repeat password have to be the same/i)).toBeInTheDocument();
     });
 });
