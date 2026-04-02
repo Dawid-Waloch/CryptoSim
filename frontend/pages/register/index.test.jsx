9cf7e74f26065a21cea89d5f60ce688e8d6737e3
Dawid <dawwal1900@gmail.com>
2026-04-02 15:13:59 +0200

    Add renders toast when user successfully register test

--- frontend/pages/register/index.test.jsx
+++ frontend/pages/register/index.test.jsx
@@ -1,9 +1,21 @@
 import { describe, expect, it, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
-import RegisterPage from ".";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { useToast } from "../../context/ToastContext";
+import RegisterPage from ".";
+import { useRouter } from "next/router";
 
 describe('Register Page Integration', () => {
+    const mockUser = {
+        username: 'dawid',
+        email: 'dawid@wp.pl',
+        password: '12345678'
+    };
+
+    const mockSetFlashMessage = vi.fn();
+
+    const mockPush = vi.fn();
+
     it('renders all kind of errors', async () => {
         render(<RegisterPage />);
 
@@ -17,5 +29,44 @@ describe('Register Page Integration', () => {
         expect(screen.getByText(/your email has to include @/i)).toBeInTheDocument();
         expect(screen.getByText(/your password has to contain more than 7 characters/i)).toBeInTheDocument();
         expect(screen.getByText(/you password and repeat password have to be the same/i)).toBeInTheDocument();
+    });
+
+    it('renders toast when user successfully register', async () => {
+        vi.mocked(useToast).mockReturnValue({
+            setFlashMessage: mockSetFlashMessage
+        });
+
+        vi.mocked(useRouter).mockReturnValue({
+            push: mockPush
+        });
+
+        render(<RegisterPage />);
+
+        fetch.mockResolvedValueOnce({
+            ok: true,
+            status: 200,
+            json: async () => ({ success: true })
+        });
+
+        const usernameInput = screen.getByPlaceholderText('Username');
+        const emailInput = screen.getByPlaceholderText('E-mail');
+        const passwordInput = screen.getByPlaceholderText('Password');
+        const repeatPasswordInput = screen.getByPlaceholderText('Repeat password');
+        const submitButton = screen.getByRole('button', { name: /submit/i });
+
+        await userEvent.type(usernameInput, mockUser.username);
+        await userEvent.type(emailInput, mockUser.email);
+        await userEvent.type(passwordInput, mockUser.password);
+        await userEvent.type(repeatPasswordInput, mockUser.password);
+        await userEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(mockSetFlashMessage).toHaveBeenCalledWith({
+                type: 'success',
+                message: 'Registration success'
+            });
+        });
+
+        expect(mockPush).toHaveBeenCalledWith('/login');
     });
 });
