08141437709cc354779a4cbfc105e09a25443944
Dawid <dawwal1900@gmail.com>
2026-04-13 20:24:49 +0200

    Add login.spec.js file with login e2e test

---
+++ frontend/tests/e2e/auth/login.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+import { deleteTestUser, registerAndloginTestUser } from '../../db-utils';
+
+test.describe('Login', () => {
+    let testUser;
+
+    test.beforeAll(async ({ request }) => {
+        testUser = await registerAndloginTestUser({ request });
+    });
+
+    test.afterAll(async ({ request }) => {
+        await deleteTestUser({
+            request,
+            username: testUser.user.username,
+        });
+    });
+
+    test('allows user to log in with valid credentials', async ({ page }) => {
+        await page.goto('/login');
+        await page.getByTestId('login-username-input').fill(testUser.user.username);
+        await page.getByTestId('login-password-input').fill(testUser.user.password);
+        await page.getByTestId('login-submit-btn').click();
+        await expect(page).toHaveURL('/dashboard');
+    });
+});
