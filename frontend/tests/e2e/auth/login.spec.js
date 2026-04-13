37f36d6fe903a6384634108930c084fe1ad4ce5f
Dawid <dawwal1900@gmail.com>
2026-04-13 20:59:51 +0200

    Add test where is checked if user is not logged out after page refresh to e2e login tests file

--- frontend/tests/e2e/auth/login.spec.js
+++ frontend/tests/e2e/auth/login.spec.js
@@ -31,4 +31,14 @@ test.describe('Login', () => {
         await expect(page.getByText(/user not found/i)).toBeVisible({ timeout: 4000 });
         await expect(page).toHaveURL('/login');
     });
+
+    test('does not logging out after page refresh', async ({ page }) => {
+        await page.goto('/login');
+        await page.getByTestId('login-username-input').fill(testUser.user.username);
+        await page.getByTestId('login-password-input').fill(testUser.user.password);
+        await page.getByTestId('login-submit-btn').click();
+        await expect(page).toHaveURL('/dashboard');
+        await page.reload();
+        await expect(page).toHaveURL('/dashboard');
+    });
 });
