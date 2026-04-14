ca7ef3f0ac05c9a6306815b327a91ce3b5976dbd
Dawid <dawwal1900@gmail.com>
2026-04-14 21:05:59 +0200

    Fix login e2e flaky tests

--- frontend/tests/e2e/auth/login.spec.js
+++ frontend/tests/e2e/auth/login.spec.js
@@ -4,11 +4,11 @@ import { deleteTestUser, registerTestUser } from '../../db-utils';
 test.describe('Login', () => {
     let testUser;
 
-    test.beforeAll(async ({ request }) => {
+    test.beforeEach(async ({ request }) => {
         testUser = await registerTestUser({ request });
     });
 
-    test.afterAll(async ({ request }) => {
+    test.afterEach(async ({ request }) => {
         await deleteTestUser({
             request,
             username: testUser.user.username,
@@ -17,10 +17,16 @@ test.describe('Login', () => {
 
     test('allows user to log in with valid credentials', async ({ page }) => {
         await page.goto('/login');
+
         await page.getByTestId('login-username-input').fill(testUser.user.username);
         await page.getByTestId('login-password-input').fill(testUser.user.password);
-        await page.getByTestId('login-submit-btn').click();
-        await expect(page).toHaveURL('/dashboard');
+
+        await Promise.all([
+            page.waitForURL('/dashboard'),
+            page.getByTestId('login-submit-btn').click()
+        ]);
+
+        await expect(page.getByText(/welcome/i)).toBeVisible();
     });
 
     test('refuses user to log in with invalid credentials', async ({ page }) => {
@@ -28,17 +34,24 @@ test.describe('Login', () => {
         await page.getByTestId('login-username-input').fill('wrongUser');
         await page.getByTestId('login-password-input').fill('wrongUser123');
         await page.getByTestId('login-submit-btn').click();
-        await expect(page.getByText(/user not found/i)).toBeVisible({ timeout: 4000 });
+
+        await expect(page.getByText(/user not found/i)).toBeVisible();
         await expect(page).toHaveURL('/login');
     });
 
     test('does not logging out after page refresh', async ({ page }) => {
         await page.goto('/login');
+
         await page.getByTestId('login-username-input').fill(testUser.user.username);
         await page.getByTestId('login-password-input').fill(testUser.user.password);
-        await page.getByTestId('login-submit-btn').click();
-        await expect(page).toHaveURL('/dashboard');
+
+        await Promise.all([
+            page.waitForURL('/dashboard'),
+            page.getByTestId('login-submit-btn').click()
+        ]);
+
         await page.reload();
+
         await expect(page).toHaveURL('/dashboard');
     });
 });
