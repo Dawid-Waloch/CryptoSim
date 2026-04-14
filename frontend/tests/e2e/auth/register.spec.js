cf1507a126b4396dfe68176c29a032db657294a8
Dawid <dawwal1900@gmail.com>
2026-04-14 20:17:01 +0200

    Add reigster.spec.js file and register user with valid credentials test into it

---
+++ frontend/tests/e2e/auth/register.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+import { createUser, deleteTestUser } from '../../db-utils';
+
+test.describe('Register', () => {
+    const mockUser = createUser();
+
+    test.afterEach(async ({ request }) => {
+        await deleteTestUser({ request, username: mockUser.username });
+    })
+
+    test('Register user with valid credentials', async ({ page, request }) => {
+        await page.goto('/register');
+        await page.getByTestId('register-username-input').fill(mockUser.username);
+        await page.getByTestId('register-email-input').fill(mockUser.email);
+        await page.getByTestId('register-password-input').fill(mockUser.password);
+        await page.getByTestId('register-repeat-password-input').fill(mockUser.password);
+        await page.getByTestId('register-submit-btn').click();
+
+        await expect(page.getByText(/registration success/i)).toBeVisible({ timeout: 4000 });
+    });
+});
