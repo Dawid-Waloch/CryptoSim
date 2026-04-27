99dea0436ca08d77bb26df5ce88e6a737b3e888f
Dawid <dawwal1900@gmail.com>
2026-04-27 20:41:16 +0200

    Add Dashboard e2e test file and 2 tests testing if dashboard is loading and if wallet balance is loading

---
+++ frontend/tests/e2e/core/dashboard.spec.js
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+import { deleteTestUser, registerAndLoginTestUser } from '../../db-utils';
+
+test.describe('Dashboard', () => {
+    const quantity = 1;
+    let testUser;
+    let asset;
+    let usdWalletBalance;
+    let walletBalanceAfterBuy;
+
+    test.beforeEach(async ({ request, page }) => {
+        const { data, user } = await registerAndLoginTestUser({ request, page });
+        testUser = user;
+
+        const assets = await request.get('http://localhost:8080/assets');
+        const assetsList = await assets.json();
+        asset = assetsList[0];
+        
+
+        const wallets = await request.post(`http://localhost:8080/wallets/${data.userId}`);
+        const walletsList = await wallets.json();
+        usdWalletBalance = walletsList.wallets.find(w => w.currency === "USD").balance;
+        walletBalanceAfterBuy = usdWalletBalance - (asset.currentPrice * quantity);
+
+        await request.post('http://localhost:8080/transactions/buy', { data: 
+            { userId: data.userId, assetId: asset.id, quantity: quantity }
+        });
+    });
+
+    test.afterEach(async ({ request }) => {
+        await deleteTestUser({
+            request,
+            username: testUser.username
+        });
+    });
+    
+    test('loads dashboard correctly', async ({ page }) => {
+        await page.goto('/dashboard');
+        await expect(page.getByText(/welcome/i)).toBeVisible();
+    });
+
+    test('renders data correctly', async ({ page }) => {
+        await page.goto('/dashboard');
+        await page.getByTestId('dashboard-select').click();
+        await page.getByTestId('dashboard-menu-item').click();
+        await expect(page.getByTestId('dashboard-wallet-balance')).toContainText(walletBalanceAfterBuy.toString());
+    });
+});
